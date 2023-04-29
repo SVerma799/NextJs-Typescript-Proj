a9a524bd16192390d6228fa85672766846706953
Shubham Verma <vshubham799@gmail.com>
2023-04-28 17:22:43 -0700

    Completed Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,37 @@
-import Image from "next/image";
+import type { Metadata } from "next";
+import LargeHeading from "@/components/ui/LargeHeading";
 import Paragraph from "./components/ui/Paragraph";
-import LargeHeading from "./components/ui/LargeHeading";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "Home",
+  description: "Free and open source text similiarity checker app",
+};
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <div className="relative h-screen flex items-center justify-center overflow-x-hidden">
+      <div className="container pt-32 w-full h-full">
+        <div className="h-full gap-6 flex flex-col justify-start lg:justify-center items-center lg-items-start">
+          <LargeHeading
+            size={"lg"}
+            className="three-d text-black dark:text-light-gold"
+          >
+            Easily determine <br /> text similiarity
+          </LargeHeading>
+
+          <Paragraph className="max-w-xl lg:text-left">
+            with the text similarity API, you can easily determine the
+            similarity between two pieces of text with a free{" "}
+            <Link
+              href="/login"
+              className="underline underline-offset-2 text-black dark:text-light-gold"
+            >
+              API Key
+            </Link>
+          </Paragraph>
+        </div>
+      </div>
+    </div>
+  );
 }
